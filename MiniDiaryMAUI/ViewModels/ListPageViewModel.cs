167eca527097281624a956ffc3916781aa15e788
Muffine123 <kglab.078@gmail.com>
2026-09-18 11:33:26 +0200

    added displaying list of items

--- MiniDiaryMAUI/ViewModels/ListPageViewModel.cs
+++ MiniDiaryMAUI/ViewModels/ListPageViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MiniDiaryMAUI.Models;
+using MiniDiaryMAUI.Services;
+using System.Collections.ObjectModel;
 
 namespace MiniDiaryMAUI.ViewModel;
 
 [QueryProperty(nameof(Mode), "mode")]
 public partial class ListPageViewModel : ObservableObject
 {
+    private readonly IEntriesService _entriesService;
     [ObservableProperty]
     private string mode;
 
@@ -15,6 +19,38 @@ public partial class ListPageViewModel : ObservableObject
     [ObservableProperty]
     private bool isWeights;
 
+    [ObservableProperty]
+    private ObservableCollection<EntryWeight> listWeights;
+
+    [ObservableProperty]
+    private ObservableCollection<EntryNote> listNotes;
+
+    public ListPageViewModel(IEntriesService entriesService)
+    {
+        _entriesService = entriesService;
+    }
+    public async Task LoadAllEntriesAsync()
+    {
+        var allWeights = await _entriesService.GetWeightsAsync();
+        if (allWeights != null)
+        {
+
+            ListWeights = new ObservableCollection<EntryWeight>();
+            foreach (var weight in allWeights)
+            {
+                ListWeights.Add(weight);
+            }
+        }
+        var allNotes = await _entriesService.GetNotesAsync();
+        if (allNotes != null)
+        {
+            ListNotes = new ObservableCollection<EntryNote>();
+            foreach (var note in allNotes)
+            {
+                ListNotes.Add(note);
+            }
+        }
+    }
     partial void OnModeChanged(string value)
     {
         if (value == "notes")
--- MiniDiaryMAUI/Views/ListPage.xaml.cs
+++ MiniDiaryMAUI/Views/ListPage.xaml.cs
@@ -1,12 +1,32 @@
-using MiniDiaryMAUI.Models;
+using MiniDiaryMAUI.Services;
+using MiniDiaryMAUI.ViewModel;
 
 namespace MiniDiaryMAUI.Views;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
+    private readonly ListPageViewModel _viewModel;
+    public ListPage(IEntriesService entriesService)
 	{
 		InitializeComponent();
-		BindingContext = new ViewModel.ListPageViewModel();
-	}
+
+        _viewModel = new ListPageViewModel(entriesService);
+        BindingContext = _viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Console.WriteLine("MAIN PAGE APPEARING");
+
+        try
+        {
+            await _viewModel.LoadAllEntriesAsync();
+            Console.WriteLine("LOAD FINISHED");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"APPEARING ERROR: {ex}");
+        }
+    }
 }
