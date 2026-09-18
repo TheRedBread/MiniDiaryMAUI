9dfbd5ca793566243ac50d9c46704313e445e3ee
RedBread <trefel.yt@gmail.com>
2026-09-18 11:45:02 +0200

    styling

--- MiniDiaryMAUI/AppShell.xaml.cs
+++ MiniDiaryMAUI/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace MiniDiaryMAUI
+﻿using MiniDiaryMAUI.Views;
+
+namespace MiniDiaryMAUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("list", typeof(ListPage));
+
         }
     }
 }
